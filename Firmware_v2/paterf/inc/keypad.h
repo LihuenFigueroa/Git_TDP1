ef61acf5541c6ac33255f755ee68a690df97153f
Santiago <santiagocorrao96@outlook.com>
2018-12-18 01:25:26 -0300

    Keypad Listo, Falta testear

--- Firmware_v2/paterf/inc/keypad.h
+++ Firmware_v2/paterf/inc/keypad.h
@@ -9,12 +9,12 @@
 
 #define _KEYPAD_H
 
-#define SIZE_BUFFER_IN 3
+#define SIZE_BUFFER_IN 4
 
 	void KEYPAD_Init(void);
-	void KEYPAD_Read_Buffer(unsigned char outBuffer[SIZE_BUFFER_IN]);
+	void KEYPAD_Read_Buffer(uint16_t outBuffer[SIZE_BUFFER_IN]);
 	void KEYPAD_Buffer_Clear(void) ;
 	void KEYPAD_Interrupt(void);
-	unsigned char KEYPAD_LastKey(void);
+	uint16_t KEYPAD_LastKey(void);
 
 #endif /* KEYPAD_H_ */
