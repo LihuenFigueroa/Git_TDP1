848951ca1b9f3db6a8b98ddb76f7159aa7953bc6
Santiago <santiagocorrao96@outlook.com>
2018-12-18 15:05:48 -0300

    COMPILAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ Firmware_v2/Shi_Zeijer/inc/main_MEF.h
@@ -0,0 +1,18 @@
+/*
+ * main_MEF.h
+ *
+ *  Created on: 18/12/2018
+ *      Author: Lihuen
+ */
+
+#ifndef MAIN_MEF_H_
+#define MAIN_MEF_H_
+
+#include "sapi.h"
+
+ void main_MEF_Init();
+ void main_MEF_Update();
+
+
+
+#endif /* MAIN_MEF_H_ */
---
+++ Firmware_v2/Shi_Zeijer/inc/rs485.h
@@ -0,0 +1,13 @@
+#ifndef _RS485_H
+
+#define _RS485_H
+
+#include "comm.h"
+
+
+uint8_t RS485_Flag();
+void    RS485_GetBuffer     (uint8_t buffer[SIZE_BUFFER]);
+void    RS485_SetState      (estado_t state);
+void    RS485_SetBaudRate   (uint32_t baudRate);
+void    RS485_Write         (uint8_t *data);
+#endif
