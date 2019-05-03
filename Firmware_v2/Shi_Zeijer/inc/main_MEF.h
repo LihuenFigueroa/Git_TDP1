fcc6560955104ea9fe9805d2b79e6e82e72babc0
LihuenFigueroa <lihuenfigueroa@gmail.com>
2019-05-03 14:09:36 -0300

    .

--- Firmware_v2/Shi_Zeijer/inc/main_MEF.h
+++ Firmware_v2/Shi_Zeijer/inc/main_MEF.h
@@ -9,10 +9,17 @@
 #define MAIN_MEF_H_
 
 #include "sapi.h"
+<<<<<<< HEAD
 
 
 void main_MEF_Init();
 void main_MEF_Update();
+=======
+
+ void main_MEF_Init();
+ void main_MEF_Update();
+
+>>>>>>> parent of 9b6704a... AGREGADO MAIN MEF
 
 
 #endif /* MAIN_MEF_H_ */
--- Firmware_v2/Shi_Zeijer/src/main_MEF.c
+++ Firmware_v2/Shi_Zeijer/src/main_MEF.c
@@ -6,10 +6,10 @@
  */
 
 #include "main_MEF.h"
-#include "board.h"
-#include "chip.h"
 #include "lcd.h"
+#include "aten.h"
 #include "comm.h"
+#include "keypad.h"
 #include "buzzer.h"
 #include "aten.h"
 #include "keypad.h"
@@ -35,6 +35,7 @@ static void fill_lcd_buffer_normal(uint8_t buffer[16]) {
 		i++;
 	}
 
+<<<<<<< HEAD
 	buffer[0] = 'A';
 	buffer[1] = 'T';
 	buffer[2] = 'E';
@@ -58,11 +59,32 @@ static void fill_lcd_buffer_att(uint8_t buffer[16]) {
 	uint8_t digitos[3] = { 0, 0, 0 };
 	uint16_t i = 0;
 	while (value != 0) {
+=======
+typedef enum{
+	STATE_NORMAL,
+	STATE_CONFIG_ATEN,
+	STATE_CONFIG_COM
+} States;
+
+static States state;
+static States oldState;
+static uint16_t key;
+static uint8_t data[32];
+static uint8_t value;
+
+/*
+static void fill_lcd_buffer_normal(uint8_t data[32]){
+	uint8_t value = Aten_Get_Actual_Aten();
+	char digitos[3];
+	int i = 0;
+	while(value != 0){
+>>>>>>> parent of 9b6704a... AGREGADO MAIN MEF
 		digitos[i] = value % 10;
 		value = value / 10;
 		i++;
 	}
 
+<<<<<<< HEAD
 	buffer[0] = 'A';
 	buffer[1] = 'T';
 	buffer[2] = 'E';
@@ -106,9 +128,48 @@ static void fill_lcd_buffer_aten_err(uint8_t buffer[16]) {
 	buffer[13] = 'A';
 	buffer[14] = ' ';
 	buffer[15] = ' ';
+=======
+	data[0] =	 'A';
+	data[1] =	 'T';
+	data[2] =	 'E';
+	data[3] =	 'N';
+	data[4] =	 'U';
+	data[5] =	 'A';
+	data[6] =	 'C';
+	data[7] =	 'I';
+	data[8] =	 'O';
+	data[9] =  'N';
+	data[10] = ' ';
+	data[11] = ':';
+	data[12] = ' ';
+	data[13] = digitos[2]+0x30;
+	data[14] = digitos[1]+0x30;
+	data[15] = digitos[0]+0x30;
+	data[16] = ' ';
+	data[17] = ' ';
+	data[18] = ' ';
+	data[19] = ' ';
+	data[20] = ' ';
+	data[21] = ' ';
+	data[22] = ' ';
+	data[23] = ' ';
+	data[24] = ' ';
+	data[25] = ' ';
+	data[26] = ' ';
+	data[27] = ' ';
+	data[28] = ' ';
+	data[29] = ' ';
+	data[30] = ' ';
+	data[31] = ' ';
+>>>>>>> parent of 9b6704a... AGREGADO MAIN MEF
 }
+*/
 
+<<<<<<< HEAD
 void main_MEF_Init() {
+=======
+void main_MEF_Init(){
+>>>>>>> parent of 9b6704a... AGREGADO MAIN MEF
 	///////////////		LCD	- INIT		/////////////////
 	LCD_init(DISPLAY_8X5 | _2_LINES, DISPLAY_ON | CURSOR_ON | CURSOR_BLINK);
 	LCD_Clear_Buffer();
@@ -125,6 +186,7 @@ void main_MEF_Init() {
 	/////////////		ATENUADOR	- INIT		/////////
 	Aten_Init();
 	/////////////////////////////////////////////////////
+<<<<<<< HEAD
 
 	/////////////		BUZZER	- INIT		/////////////
 	BUZZER_Init();
@@ -137,14 +199,29 @@ void main_MEF_Init() {
 
 void main_MEF_Update() {
 	uint8_t *param;
+=======
+	state=STATE_NORMAL;
+	oldState=STATE_NORMAL;
+}
+
+void main_MEF_Update(){
+>>>>>>> parent of 9b6704a... AGREGADO MAIN MEF
 	KEYPAD_Interrupt();
 	switch (state) {
 	case STATE_NORMAL:
+<<<<<<< HEAD
 		LCD_cursor_off();
 		LCD_cursor_blink_off();
 		key = KEYPAD_LastKey();
 		if (key) { // Si se apreto alguna tecla...
 			switch (key) {
+=======
+		//fill_lcd_buffer_normal(data);
+		LCD_Write_Buffer(data);
+		key=KEYPAD_LastKey();
+		if(key){ // Si se apreto alguna tecla...
+			switch(key){
+>>>>>>> parent of 9b6704a... AGREGADO MAIN MEF
 			case 4: // TECLA A, UN +
 				Aten_Plus();
 				fill_lcd_buffer_normal(data);
@@ -160,10 +237,16 @@ void main_MEF_Update() {
 				state = STATE_CONFIG_ATEN;
 				KEYPAD_Buffer_Number_Clear();
 				break;
+<<<<<<< HEAD
 			case 16: // TECLA D, CONFIG COMUNICACION
 				oldState = STATE_NORMAL;
 				state = STATE_CONFIG_COM;
 				com_MEF_Init();
+=======
+			case 16:// TECLA D, CONFIG COMUNICACION
+				oldState=STATE_NORMAL;
+				state=STATE_CONFIG_COM;
+>>>>>>> parent of 9b6704a... AGREGADO MAIN MEF
 				break;
 			default:
 				oldState = STATE_NORMAL;
@@ -171,10 +254,43 @@ void main_MEF_Update() {
 				break;
 			}
 		}
+<<<<<<< HEAD
 		if (COMM_CheckSerials(&param)){
 			switch (param[0]) {
 				case '+':
 					Aten_Plus();
+=======
+		else{ // Si no se apreto alguna tecla...
+			//tengo que escuchar comunicacion
+			COMM_CheckSerials();
+		}
+		break;
+	case STATE_CONFIG_ATEN:
+		key=KEYPAD_LastKey();
+			if(key){ // Si se apreto alguna tecla...
+				switch(key){
+				case 4: // TECLA A, UN ACEPTAR
+					value=KEYPAD_GetValue();
+					// debo validarlo primero //
+					if(value <= MAX_ATEN){
+						Aten_SetValue(value);
+						oldState=STATE_CONFIG_ATEN;
+						state=STATE_NORMAL;
+					}
+					else{ // NO ES UN VALOR DE ATENUACION VALIDO
+						KEYPAD_Buffer_Number_Clear();
+						//fill_lcd_buffer_aten_err(data); // ACA ADENTRO CONVIERTO EN ASCII LOS NUMEROS DEL TECLADO
+						LCD_Write_Buffer(data);
+						BUZZER_Ring();
+						oldState=STATE_CONFIG_ATEN;
+						state=STATE_CONFIG_ATEN;
+					}
+					break;
+				case 8:// TECLA B, UN CANCELAR
+					KEYPAD_Buffer_Number_Clear();
+					oldState=STATE_CONFIG_ATEN;
+					state=STATE_NORMAL;
+>>>>>>> parent of 9b6704a... AGREGADO MAIN MEF
 					break;
 				case '-':
 					Aten_Minus();
@@ -182,6 +298,20 @@ void main_MEF_Update() {
 				default:
 					Aten_SetValue(atoi(param));
 					break;
+<<<<<<< HEAD
+=======
+				case 15:// TECLA #, NOP
+					oldState=STATE_CONFIG_ATEN;
+					state=STATE_CONFIG_ATEN;
+					break;
+				default: // CUALQUIER NUMERO XD
+					//fill_lcd_buffer_aten(data); // ACA ADENTRO CONVIERTO EN ASCII LOS NUMEROS DEL TECLADO
+					LCD_Write_Buffer(data);
+					oldState=STATE_CONFIG_ATEN;
+					state=STATE_CONFIG_ATEN;
+					break;
+				}
+>>>>>>> parent of 9b6704a... AGREGADO MAIN MEF
 			}
 			fill_lcd_buffer_normal(data);
 			LCD_Write_Buffer_Line(data, 0);
@@ -242,6 +372,7 @@ void main_MEF_Update() {
 		break;
 
 	case STATE_CONFIG_COM:
+<<<<<<< HEAD
 		key = KEYPAD_LastKey();
 		if (key == 16) { // Si me tocaron una tecla Y esa fue la D...
 			oldState = STATE_CONFIG_COM;
@@ -252,6 +383,9 @@ void main_MEF_Update() {
 		} else {
 			com_MEF_Update(key);
 		}
+=======
+
+>>>>>>> parent of 9b6704a... AGREGADO MAIN MEF
 		break;
 	}
 
